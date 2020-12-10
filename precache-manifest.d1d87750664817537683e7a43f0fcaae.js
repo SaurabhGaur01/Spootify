c91800d1da3fbac2c975ec3b8d5f87d1541aa7b2
Saurabh Gaur <saurabh.gaur01@gmail.com>
2020-12-10 23:49:49 +0000

    Updates

---
+++ precache-manifest.d1d87750664817537683e7a43f0fcaae.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08487d0f5e1b5b405d75c9ad13c888d6",
+    "url": "/Spootify/index.html"
+  },
+  {
+    "revision": "0e881cf8e9acd4164be9",
+    "url": "/Spootify/static/css/2.72994648.chunk.css"
+  },
+  {
+    "revision": "d54497d01b42e90fb46e",
+    "url": "/Spootify/static/css/main.a2659e39.chunk.css"
+  },
+  {
+    "revision": "0e881cf8e9acd4164be9",
+    "url": "/Spootify/static/js/2.72e39be2.chunk.js"
+  },
+  {
+    "revision": "a7aec927f1e863a53de22e58e5329447",
+    "url": "/Spootify/static/js/2.72e39be2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d54497d01b42e90fb46e",
+    "url": "/Spootify/static/js/main.c1838888.chunk.js"
+  },
+  {
+    "revision": "e45338e98abce4244bec",
+    "url": "/Spootify/static/js/runtime-main.c16a3168.js"
+  },
+  {
+    "revision": "8d5ea91351989c394b92ea05d5019c82",
+    "url": "/Spootify/static/media/avatar.8d5ea913.svg"
+  },
+  {
+    "revision": "ee1d2f309079138dcfdbd52521b250fe",
+    "url": "/Spootify/static/media/hero.ee1d2f30.svg"
+  }
+]);
