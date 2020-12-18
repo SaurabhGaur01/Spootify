7ba866128d03a3025caabdb0c45d0690f8e45661
SaurabhGaur01 <68402470+SaurabhGaur01@users.noreply.github.com>
2020-12-18 22:25:04 +0000

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ export default {
   api: {
     baseUrl: 'https://api.spotify.com/v1',
     authUrl: 'https://accounts.spotify.com/api/token',
-    clientId: '19af5d8293e34205ab72c042b187c695',
-    clientSecret: '17382941b9804649a882611e59a13f23'
+    clientId: '',
+    clientSecret: ''
   }
 }
